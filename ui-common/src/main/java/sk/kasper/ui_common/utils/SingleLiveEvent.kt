d201e1c096edd64a972313ad63346bc69f344b89
MoltenEQ <molteneq.official@gmail.com>
2022-05-15 14:28:36 +0200

    Fixed security issues

--- ui-common/src/main/java/sk/kasper/ui_common/utils/SingleLiveEvent.kt
+++ ui-common/src/main/java/sk/kasper/ui_common/utils/SingleLiveEvent.kt
@@ -5,6 +5,7 @@ import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import timber.log.Timber
 import java.util.concurrent.atomic.AtomicBoolean
 
 /**
@@ -25,7 +26,7 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
 
     override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
         if (hasActiveObservers()) {
-            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+            Timber.w(TAG, "Multiple observers registered but only one will be notified of changes.")
         }
 
         // Observe the internal MutableLiveData
